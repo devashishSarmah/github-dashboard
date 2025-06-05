70572356826170cb1b077eeb9b3ee3a1ec4f2f08
Devashish Sarmah <devashish.dev.sarmah@gmail.com>
2025-06-05 21:50:37 +0530

    feat: responsiveness check

--- src/elements/user-profile-card/user-profile-card.spec.ts
+++ src/elements/user-profile-card/user-profile-card.spec.ts
@@ -1,23 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { UserProfileCard } from './user-profile-card';
+import { UserProfileCard } from "./user-profile-card";
 
-describe('UserProfileCard', () => {
+describe("UserProfileCard", () => {
   let component: UserProfileCard;
   let fixture: ComponentFixture<UserProfileCard>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserProfileCard]
-    })
-    .compileComponents();
+      imports: [UserProfileCard],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserProfileCard);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -3,10 +3,14 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { Home } from "./home";
 import { config } from "../../config";
 import { userSearchStore } from "../../stores/github-store";
+import { ComponentRef, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe("Home", () => {
   let component: Home;
   let fixture: ComponentFixture<Home>;
+  let componentRef: ComponentRef<Home>;
+  let debugEl: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +20,23 @@ describe("Home", () => {
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    componentRef = fixture.componentRef;
+    debugEl = fixture.debugElement;
+
+    component.query.set("");
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should call search method when query has been typed and search button is clicked", () => {
+    const searchSpy = jasmine.createSpy("search");
+    component.search = searchSpy;
+    component.query.set("angular");
+    fixture.detectChanges();
+    const searchButton = debugEl.query(By.css("#searchBtn"));
+    searchButton.triggerEventHandler("click", null);
+    expect(searchSpy).toHaveBeenCalled();
   });
 });
